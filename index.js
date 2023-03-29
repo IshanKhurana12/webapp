343978d6bd3923d2faa6bbfc0826c0f011d02828
ishan2op <ishankhurana176@gmail.com>
2023-03-29 18:02:24 +0530

    second commit adding routes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express=require('express');
 const app=express();
-
-
 const port=8000;
 
+
+//use exxpress router
+app.use('/',require('./routes/index'));
 app.listen(port,function(err){
     if(err){
         console.log(`error in eunning the server: ${err}`);
     }
     console.log(`server is running on port :${port}`);
-})
+});
+ 
